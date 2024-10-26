d95d511f4bea6fa5d2a8484993f20435119c5463
samarrebhi <samar.rebhi@esprit.tn>
2024-10-26 14:19:54 +0100

    validators in forms

--- apps/zonegéosamarapp/models.py
+++ apps/zonegéosamarapp/models.py
@@ -1,16 +1,15 @@
+# models.py
 from django.db import models
 
 # Create your models here.
 class ZoneGeographique(models.Model):
-   
-    nomZone = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True, null=True)  
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)  
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)  
+    nomZone = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    annee = models.PositiveIntegerField()
+    temperatureMoyenne = models.DecimalField(max_digits=5, decimal_places=2)
+    pluviometrie = models.DecimalField(max_digits=6, decimal_places=2)
 
-    annee = models.PositiveIntegerField() 
-    temperatureMoyenne = models.DecimalField(max_digits=5, decimal_places=2) 
-    pluviometrie = models.DecimalField(max_digits=6, decimal_places=2)  
-    
-def __str__(self):
+    def __str__(self):
         return self.nomZone
--- apps/zonegéosamarapp/templatetags/custom_filters.py
+++ apps/zonegéosamarapp/templatetags/custom_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 def add_class(field, css_class):
     """Add a class to a form field."""
     return field.as_widget(attrs={'class': css_class})
+
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    return field.as_widget(attrs={"class": css_class})
